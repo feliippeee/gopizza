ff28dea6b0b67a50ae8ead4a6e228c204bd4bf73
Felipe Marques <felipe.tecnosuporte@gmail.com>
2022-01-29 18:19:02 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useFonts, DMSans_400Regular} from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components/native';
+// falta configuração pro ios
+import theme from "./src/theme";
+
+import { SignIn} from './src/screens/SignIn';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+  return (
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
+  );
+}
